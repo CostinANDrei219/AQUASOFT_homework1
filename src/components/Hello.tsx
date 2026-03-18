b45f047ebbf9c5725bd882cfbf6a52b9e355db6f
CostinANDrei119 <costin.sararu@stud.acs.upb.ro>
2026-03-18 19:39:06 +0200

    Small changes and added index.ts to the scripts

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -6,16 +6,20 @@ export default function Hello() {
     const [car, setCar] = useState('BMW')
     const inputRef = useRef<HTMLInputElement | null>(null)
 
-    const handleChange = () => {
-        const val = inputRef.current?.value
-        if (val) setCar(val)
+    const changeCar = () => {
+        const val = inputRef.current.value
+        if(typeof val === 'string')
+        {
+            setCar(val)
+        }
+            
     }
 
     return (
-        <div style={{ fontFamily: 'system-ui, sans-serif', padding: 16 }}>
+        <div >
             <h2>My favorite car is: {car}</h2>
             <input ref={inputRef} type="text" placeholder="Enter a car" />
-            <button onClick={handleChange} style={{ marginLeft: 8 }}>
+            <button onClick={changeCar} style={{ marginLeft: 8 }}>
                 Change Car
             </button>
         </div>
